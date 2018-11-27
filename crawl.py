0996b5d0609d23e557216fab49f648cb105d3c83
Hyeock Jin <kherootz@gmail.com>
2018-11-27 14:04:38 +0900

    Add unverified_context

--- crawl.py
+++ crawl.py
@@ -1,11 +1,11 @@
 import json
 import pickle
+import ssl
 import time
 from urllib.request import Request, urlopen
 
 topic_list = []
 topic_dic = {}
-
 
 def read_json(x):
     refer_head = 'http://www.google.com/'
@@ -13,13 +13,14 @@ def read_json(x):
     user_head = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome'
     head = 'application/vnd.github.mercy-preview+json'
     url = 'https://api.github.com/search/repositories?q=topic:'
-
+    context = ssl._create_unverified_context()
+    
     request = Request(url + x)
     request.add_header('User-Agent', user_head)
     request.add_header('referer', refer_head)
     request.add_header('Connection', connection_head)
     request.add_header('Accept', head)
-    response = urlopen(request)
+    response = urlopen(request, context=context)
     topic_dic[x] = {}
     for data in json.load(response)['items']:
         for topic in data['topics']:
